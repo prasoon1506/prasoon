345f65ef092d811d601e71ca5d5dcb86d1dea1dd
prasoon1506 <121571475+prasoon1506@users.noreply.github.com>
2024-12-05 14:18:18 +0530

    Update price.py

--- price.py
+++ price.py
@@ -177,7 +177,6 @@ def save_processed_dataframe(df):
     return output
 
 def main():
-    st.title("Price Tracker")
     st.set_page_config(page_title="Price Tracker", layout="wide")
     # File Uploader
     uploaded_file = st.file_uploader("Please upload the Price Tracker file", type=['xlsx'])
