a19ce8a17b2b6886870a478ed04bc2e2545c2ca5
prasoon1506 <121571475+prasoon1506@users.noreply.github.com>
2024-12-09 15:05:49 +0530

    Update price.py

--- price.py
+++ price.py
@@ -402,14 +402,19 @@ def main():
                         
                         # Button to add new rows
                         if st.button("Add New Rows to Dataframe"):
-                            new_rows_df = pd.DataFrame(data_entries)
-                            for col in df.columns:
-                              if col not in new_rows_df.columns:
-                               new_rows_df[col] = None
-                            new_rows_df = new_rows_df.reindex(columns=df.columns)
-                            df = pd.concat([df, new_rows_df], ignore_index=True)
-    
-                            st.success(f"{len(data_entries)} new rows added successfully!")
+                           new_rows_df = pd.DataFrame(data_entries)
+                           for col in df.columns:
+                             if col not in new_rows_df.columns:
+                                new_rows_df[col] = None
+                           new_rows_df = new_rows_df.reindex(columns=df.columns)
+                           for col in df.columns:
+                             if df[col].dtype != new_rows_df[col].dtype:
+                              try:
+                                 new_rows_df[col] = new_rows_df[col].astype(df[col].dtype)
+                              except:
+                                 pass
+                           df = pd.concat([df, new_rows_df], ignore_index=True)
+                           st.success(f"{len(data_entries)} new rows added successfully!")
             with col2:
                 st.subheader("📈 Region Analysis")
                 
