1b36fecc132ec72cb796027feb7f64b2a4d2e70b
prasoon1506 <121571475+prasoon1506@users.noreply.github.com>
2024-12-09 15:47:12 +0530

    Update price.py

--- price.py
+++ price.py
@@ -402,7 +402,7 @@ def main():
                            for col in df.columns:
                              if col not in new_rows_df.columns:
                                 new_rows_df[col] = None
-                           new_rows_df = new_rows_df.reindex(columns=df.columns)
+                          new_rows_df = new_rows_df.reindex(columns=df.columns)
                           df = pd.concat([df, new_rows_df], ignore_index=True)
                           st.success(f"{len(data_entries)} new rows added successfully!")
             with col2:
